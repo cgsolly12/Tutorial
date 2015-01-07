d2135fe870c484366a5b6d4dcd9ba54041299048
cgsolly12 <cgsolly12@gmail.com>
2015-01-07 16:49:59 -0500

    Create CharlesSolly1.py

---
+++ CharlesSolly1.py
@@ -0,0 +1,17 @@
+#Charlie#
+#title#
+import os
+import sys
+q1=raw_input"Hi what's your name?"
+q2=raw_input"Where do you live?"
+q3=raw_input"How old are you?"
+q4=raw_input"What school do you go to?"
+q5=raw_input"What's your favorite food?"
+q6=input"Cool, nice to meet you"+ q1
+print "Welcome to robotics."
+if q5==pizza:
+  print "Me too."
+elif q2=="Haddon Heights" while q4=="CCTS":
+  print "You're just like me!"
+else:
+  print "That's cool."
